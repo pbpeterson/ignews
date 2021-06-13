dda6e717114606209a0251b21ded0c2cd0b886fc
Peterson Pereira <petersonbozza7@gmail.com>
2021-06-12 23:25:46 -0300

    adicionado ignews

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SingInButton } from '../SingInButton'
+import styles from './styles.module.scss'
+
+export function Header(){
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ignews" />
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SingInButton/>
+      </div>
+    </header>
+  )
+}
