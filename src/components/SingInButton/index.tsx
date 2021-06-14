d1ef189d874eb25bbb4a2ecd149ba6504c8cee6f
Peterson Pereira <petersonbozza7@gmail.com>
2021-06-14 04:41:18 -0300

    adicionado login com github

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,22 +1,28 @@
 import styles from "./styles.module.scss"
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import {signIn, signOut, useSession} from 'next-auth/client'
 
 export function SingInButton(){
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+
+  console.log(session)
+
+  return session ? (
     <button 
     type='button'
     className={styles.signInButton}
+    onClick={()=>signOut()}
     >
       <FaGithub color='#04d461'/>
-      Peterson Pereira
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
     type='button'
     className={styles.signInButton}
+    onClick={()=>signIn('github')}
     >
       <FaGithub color='#eba417'/>
       Sign in with Github
